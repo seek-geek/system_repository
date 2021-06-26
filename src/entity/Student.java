e3310bdbe306d6a24558ce6640898efe1cf1068c
seek-geek <2363550687@qq.com>
2021-06-26 20:26:39 +0800

    学生类添加了name和age

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -2,4 +2,22 @@ package entity;
 
 public class Student {
     //添加了一个学生类
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
